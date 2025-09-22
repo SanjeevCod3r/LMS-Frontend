37269b2f2730e98a27285617f44da159e88afaa4
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-09-22 20:21:29 +0530

    [Sanjeev Jaiswal] Add: DONE

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,58 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { 
-  FacebookLogo, 
-  TwitterLogo, 
   LinkedinLogo, 
-  GithubLogo, 
-  GitBranch, 
-  WhatsappLogo 
+  WhatsappLogo,
+  InstagramLogo
 } from 'phosphor-react'
 
 const SocialIcons = () => {
   return (
-    <div className='flex items-center gap-3 mt-5 ml-1 mb-2 max-md:mt-4'>
+    <div className='flex items-center gap-4 mt-5 ml-1 mb-2 max-md:mt-4'>
+      {/* LinkedIn */}
       <Link 
         target='_blank' 
-        to='https://www.facebook.com/profile.php?id=100026766931684'
-        className="group transition transform hover:scale-110 text-[#0b16f1] hover:text-blue-500"
+        to='https://www.linkedin.com/in/naimish-sri/'
+        className="group transition transform hover:scale-110 text-white hover:text-blue-400"
+        title="Connect on LinkedIn"
       >
-        <FacebookLogo size={34} weight="fill" className="transition-colors duration-300" />
+        <LinkedinLogo size={32} weight="fill" className="transition-colors duration-300" />
       </Link>
+      
+      {/* WhatsApp */}
       <Link 
         target='_blank' 
-        to='https://x.com/gps_96169'
-        className="group transition transform hover:scale-110 text-[#cbc3c3] hover:text-gray-700"
+        to='https://wa.link/dv21xs'
+        className="group transition transform hover:scale-110 text-white hover:text-green-400"
+        title="Chat on WhatsApp"
       >
-        <TwitterLogo size={34} weight="fill" className="transition-colors duration-300" />
+        <WhatsappLogo size={32} weight="fill" className="transition-colors duration-300" />
       </Link>
+      
+      {/* Instagram */}
       <Link 
         target='_blank' 
-        to='https://www.linkedin.com/in/gyan-pratap-singh-275785236/'
-        className="group transition transform hover:scale-110 text-[#1e17ea] hover:text-blue-600"
+        to='https://www.instagram.com/naimish_sri/'
+        className="group transition transform hover:scale-110 text-white hover:text-pink-400"
+        title="Follow on Instagram"
       >
-        <LinkedinLogo size={34} weight="fill" className="transition-colors duration-300" />
-      </Link>
-      <Link 
-        target='_blank' 
-        to='https://github.com/gyanthakur'
-        className="group transition transform hover:scale-110 text-[#c2baba] hover:text-gray-500"
-      >
-        <GithubLogo size={34} weight="fill" className="transition-colors duration-300" />
-      </Link>
-      <Link 
-        target='_blank' 
-        to='https://github.com/Gyanthakur/Edemy-LMS'
-        className="group transition transform hover:scale-110 text-[#e01d1d] hover:text-red-600"
-      >
-        <GitBranch size={34} weight="fill" className="transition-colors duration-300" />
-      </Link>
-      <Link 
-        target='_blank' 
-        to='https://wa.me/918957818597?text=Hey%20%F0%9F%91%8B%2C%20how%20can%20I%20help%20you%3F'
-        className="group transition transform hover:scale-110 text-[#0fc865] hover:text-green-500"
-      >
-        <WhatsappLogo size={34} weight="fill" className="transition-colors duration-300" />
+        <InstagramLogo size={32} weight="fill" className="transition-colors duration-300" />
       </Link>
     </div>
   )
